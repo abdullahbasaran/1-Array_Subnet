36d7cdc89c3eff5801879c8bfccad3998ff88f2a
aa <asdfgh>
2019-11-25 01:16:06 +0300

    Initial Commit

---
+++ 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by aa on 25.11.2019.
+//  Copyright © 2019 aa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
